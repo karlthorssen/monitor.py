498fb52c6acaf1cd66892e1f293f939d095039fe
Karl Thorssen <karlthorssen@gmail.com>
2021-06-11 11:05:53 -0400

    docs

--- monitor_both.py
+++ monitor_both.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # forked from https://gist.github.com/PRosenb/2ce5159a334af203fca7ef06501775f0
+# routes audio from multiple blackhole channels to speakers
+# This allows me to set blackhole as system output, so I can capture it
+# in other programs and route DAW output where it needs to go.
 import sounddevice as sd
 import numpy as np
 import sys
@@ -27,7 +30,11 @@ def input_callback(indata, frames, time, status):
     output.write(np.ascontiguousarray(
         indata[:, [0,1]] + # PA
         indata[:, [2,3]] + # cue
-        indata[:, [8,9]])) # system output
+        indata[:, [8,9]])) # system output, configure as blackhole with stereo settings
+                           # i.e. set blackhole as output in system preferences, then
+                           # open audio midi setup, choose blackhole->configure speakers
+                           # set left as channel 7 and right as channel 8 (1-indexed there and
+                           # 0-indexed in numpy)
 
 with sd.InputStream(channels=16, callback=input_callback, blocksize=0, latency="low", device=IN):
     print('#' * 80)
